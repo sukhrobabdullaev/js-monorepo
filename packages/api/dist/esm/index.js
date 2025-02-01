d7a1b37d9aa2cf95ccc76b19927c054e32d5804c
Mike Purvis <mikedotexe@gmail.com>
2025-02-01 13:52:37 -0800

    telling git we'd no longer like to revision the dist/ directories in workspace packages

--- packages/api/dist/esm/index.js
+++
@@ -1,8 +0,0 @@
-/* ⋈ 🏃🏻💨 FastNEAR API - ESM (@fastnear/api version 0.6.1) */
-/* https://www.npmjs.com/package/@fastnear/api/v/0.6.1 */
-import { convertUnit } from "@fastnear/utils";
-if (typeof window !== "undefined") {
-  window.$$ = convertUnit;
-}
-export * from "./near.js";
-//# sourceMappingURL=index.js.map
--- packages/wallet-adapter-widget/dist/esm/index.d.ts
+++
@@ -1,4 +0,0 @@
-declare function handleLogin(): Promise<void>;
-declare function handleSend(): Promise<void>;
-
-export { handleLogin, handleSend };
